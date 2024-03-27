f46fdb8aa690baf93aef1775c0db02c537b2ca62
Jason Wen <jason.wen@sunnypilot.ai>
2024-03-27 16:58:05 +0000

    sunnypilot v2024.03.27-1469   version: sunnypilot v0.9.7.0 release   date: 2024-03-27T16:58:06   master commit: d22ad7ba911350c799f59a2812e7655de2dfd389

---
+++ selfdrive/manager/sunnylink.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from openpilot.common.api.sunnylink import SunnylinkApi
+from openpilot.common.spinner import Spinner
+from openpilot.system.version import is_prebuilt
+
+
+if __name__ == "__main__":
+  spinner = Spinner()
+  extra_args = {}
+  if not is_prebuilt():
+    extra_args = {
+      "verbose": True,
+      "timeout": 60
+    }
+
+  sunnylink_id = SunnylinkApi(None).register_device(spinner, **extra_args)
+  print(f"SunnyLinkId: {sunnylink_id}")
+  spinner.close()
