49a7c50d85aeef25dda3ace4f03be1c23a1ee724
1okko <15377594951@189.cn>
2024-07-03 08:32:09 +0800

    Update launch_openpilot.sh
    定制版0703

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
-
+export ATHENA_HOST='ws://opdiy.cn:5566'
+export API_HOST='http://opdiy.cn:7777'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 exec ./launch_chffrplus.sh
