8702add9e1cb7d6be45b516959f1e852439df7b6
1okko <136616964+1okko@users.noreply.github.com>
2024-07-04 08:12:47 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -2,4 +2,5 @@
 export ATHENA_HOST='ws://opdiy.cn:5566'
 export API_HOST='http://opdiy.cn:7777'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export FINGERPRINT= 'HONDA_ODYSSEY_CHN'
 exec ./launch_chffrplus.sh
