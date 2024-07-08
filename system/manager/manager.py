40775a6e9568faa80471839136d2bc0c9fe1f7c7
1okko <136616964+1okko@users.noreply.github.com>
2024-07-08 17:06:17 +0800

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -39,7 +39,7 @@ def manager_init() -> None:
     ("DisengageOnAccelerator", "0"),
     ("GsmMetered", "1"),
     ("HasAcceptedTerms", "0"),
-    ("LanguageSetting", "main_zh-CHS"),
+    ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
     ("LongitudinalPersonality", str(custom.LongitudinalPersonalitySP.standard)),
 
